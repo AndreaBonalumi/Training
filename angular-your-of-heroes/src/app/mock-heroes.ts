eb9e8ebd017ca1a2f934360a44f89b36f4f76503
Andrea Bonalumi <andrea.bonalumi@si2001.it>
2023-03-08 16:34:45 +0100

    Tour of Heroes

---
+++ angular-your-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 0, name: 'superman', visualizza: false},
+  { id: 1, name: 'batman', visualizza: false},
+  { id: 2, name: 'flash', visualizza: false},
+  { id: 3, name: 'arrow', visualizza: false},
+  { id: 4, name: 'spiderman', visualizza: false},
+  { id: 5, name: 'ironman', visualizza: false},
+  { id: 6, name: 'hulk', visualizza: false},
+  { id: 7, name: 'thor', visualizza: false},
+  { id: 8, name: 'antman', visualizza: false},
+  { id: 9, name: 'd.strange', visualizza: false},
+]
--- esercizio1/src/app/prova1/prova1.component.ts
+++ esercizio1/src/app/prova1/prova1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-prova1',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./prova1.component.css']
 })
 export class Prova1Component {
+  @Input() data: any;
 
 }
