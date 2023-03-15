905610e2e594e97ac1f93295d38109a9833bb8dd
Andrea Bonalumi <andrea.bonalumi@si2001.it>
2023-03-15 12:18:37 +0100

    table

--- Dinamic-table/src/app/pipe/pagination.pipe.ts
+++ Dinamic-table/src/app/pipe/pagination.pipe.ts
@@ -5,15 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PaginationPipe implements PipeTransform {
 
   transform(items: any[], start: number, end: number, pagination: number): any[] {
-    if(start >= 0)
-      if(start < items.length)
-        if (end > start)
-          return items.slice(start, end)
-        else
-          return []
-      else
-        return items.slice(start - pagination, start + pagination)
-    else
+    if(start < 0)
       return items.slice(0, pagination)
+    if(start >= items.length)
+      return items.slice(items.length - pagination, items.length)
+    return items.slice(start, end)
   }
 }
