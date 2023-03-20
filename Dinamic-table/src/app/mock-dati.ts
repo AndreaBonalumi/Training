e1d5636a781a58c81f5a52dccb9a777246437833
Andrea Bonalumi <andrea.bonalumi@si2001.it>
2023-03-20 17:55:36 +0100

    table

--- Dinamic-table/src/app/mock-dati.ts
+++ Dinamic-table/src/app/mock-dati.ts
@@ -2,26 +2,27 @@ import {MyTableConfig} from "./interfaces/my-table-config";
 import {MyTableActionEnum} from "./my-table-action-enum";
 
 export const   DATA : any[] = [
-  {key: 'uno', label: 'nome1'},
-  {key: 'due', label: 'nome2'},
-  {key: 'tre', label: 'nome3'},
-  {key: 'quattro', label: 'nome4'},
-  {key: 'cinque', label: 'nome5'},
-  {key: 'sei', label: 'nome6'},
-  {key: 'sette', label: 'nome7'},
-  {key: 'otto', label: 'nome8'},
-  {key: 'nove', label: 'nome9'},
-  {key: 'dieci', label: 'nome10'},
-  {key: 'unici', label: 'nome11'},
-  {key: 'dodici', label: 'nome12'},
-  {key: 'tredici', label: 'nome13'},
-  {key: 'quattordici', label: 'nome14'},
-  {key: 'quindici', label: 'nome15'},
+  {key: 'uno', label: 'nome1', ruolo: 'admin'},
+  {key: 'due', label: 'nome2', ruolo: 'admin'},
+  {key: 'tre', label: 'nome3', ruolo: 'admin'},
+  {key: 'quattro', label: 'nome4', ruolo: 'utente'},
+  {key: 'cinque', label: 'nome5', ruolo: 'utente'},
+  {key: 'sei', label: 'nome6', ruolo: 'utente'},
+  {key: 'sette', label: 'nome7', ruolo: 'utente'},
+  {key: 'otto', label: 'nome8', ruolo: 'utente'},
+  {key: 'nove', label: 'nome9', ruolo: 'utente'},
+  {key: 'dieci', label: 'nome10', ruolo: 'utente'},
+  {key: 'unici', label: 'nome11', ruolo: 'utente'},
+  {key: 'dodici', label: 'nome12', ruolo: 'utente'},
+  {key: 'tredici', label: 'nome13', ruolo: 'utente'},
+  {key: 'quattordici', label: 'nome14', ruolo: 'utente'},
+  {key: 'quindici', label: 'nome15', ruolo: 'utente'},
 ];
 export const TABLE : MyTableConfig = {
   headers: [
-    {key: 'key', label: 'Key'},
-    {key: 'label', label: 'Label'},
+    {key: 'key', label: 'Nome'},
+    {key: 'label', label: 'Cognome'},
+    {key: 'ruolo', label: 'Ruolo'}
   ],
   order: {
     colonna: 'key',
@@ -32,23 +33,30 @@ export const TABLE : MyTableConfig = {
   actions: [
     { onTop: true,
       buttonAction: {
-          text: MyTableActionEnum.NEW_ROW,
-          icon: 'postcard',
-          class: 'primary'
-        }
+        text: MyTableActionEnum.NEW_ROW,
+        icon: 'postcard',
+        class: 'primary'
+      }
     },
     { onTop: false,
       buttonAction: {
-          text: MyTableActionEnum.EDIT,
-          icon: 'pencil',
-          class: 'secondary'
-        }
+        text: MyTableActionEnum.EDIT,
+        icon: 'pencil',
+        class: 'secondary'
+      }
     },
     { onTop: false,
       buttonAction: {
         text: MyTableActionEnum.DELETE,
         icon: 'trash3',
         class: 'secondary'
+      }
+    },
+    { onTop: false,
+      buttonAction: {
+        text: 'Cambia ruolo',
+        icon: 'arrow-downward',
+        class: 'primary'
       }
     },
   ]
