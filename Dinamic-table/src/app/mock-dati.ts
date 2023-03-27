3a0058f03a2da7c2a5f382599a10fcb832256c27
Andrea Bonalumi <andrea.bonalumi@si2001.it>
2023-03-27 17:22:34 +0200

    table

--- Dinamic-table/src/app/mock-dati.ts
+++ Dinamic-table/src/app/mock-dati.ts
@@ -1,23 +1,5 @@
 import { MyTableConfig} from "./interfaces/my-table-config";
 import {MyTableActionEnum} from "./my-table-action-enum";
-
-export const   DATA : any[] = [
-  {key: 'uno', label: 'nome1', role: 'admin'},
-  {key: 'due', label: 'nome2', role: 'admin'},
-  {key: 'tre', label: 'nome3', role: 'admin'},
-  {key: 'quattro', label: 'nome4', role: 'customer'},
-  {key: 'cinque', label: 'nome5', role: 'customer'},
-  {key: 'sei', label: 'nome6', role: 'customer'},
-  {key: 'sette', label: 'nome7', role: 'customer'},
-  {key: 'otto', label: 'nome8', role: 'customer'},
-  {key: 'nove', label: 'nome9', role: 'customer'},
-  {key: 'dieci', label: 'nome10', role: 'customer'},
-  {key: 'unici', label: 'nome11', role: 'customer'},
-  {key: 'dodici', label: 'nome12', role: 'customer'},
-  {key: 'tredici', label: 'nome13', role: 'customer'},
-  {key: 'quattordici', label: 'nome14', role: 'customer'},
-  {key: 'quindici', label: 'nome15', role: 'customer'},
-];
 export const TABLE : MyTableConfig = {
   headers: [
     {key: 'id', label: 'ID'},
